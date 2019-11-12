eb48b4af0884e6380234362e50067242a40ac4a5
Nistor Cristian <contact@nistorcristian.com>
2019-11-13 00:00:27 +0200

    Improve home responsive
    Signed-off-by: Nistor Cristian <contact@nistorcristian.com>

--- themes/navy/source/js/main.js
+++ themes/navy/source/js/main.js
@@ -7,4 +7,40 @@ $(document).ready(function () {
         maxGlare: .3
     });
 
+    $(".products-container .quick-nav").stick_in_parent();
+
+    $('.quick-nav a').each(function (index, element) {
+        $(this).on('click', function (event) {
+            event.preventDefault();
+            var id = $(this).attr('href');
+            $('html, body').animate({
+                scrollTop: $(id).offset().top
+            }, 300);
+            $('.quick-nav a').removeClass('active');
+            $(this).addClass('active');
+        });
+    });
+
+    $(window).on('scroll', function(event) {
+
+        $('.in-view').each(function (index, element) {
+            if (isScrolledIntoView($(this))){
+                $('.quick-nav a').removeClass('active');
+                $('.quick-nav a[href="#'+ $(this).attr('data-target') +'"]').addClass('active');
+            }
+        });
+
+    });
+
+    // Check if element is into view when scrolling
+    function isScrolledIntoView(elem){
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(elem).height();
+
+        return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    }
+
 });
